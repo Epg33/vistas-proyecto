eb72cca6c5e5ccb83ce17cc69cc5faec885ca337
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-11-15 15:02:06 -0500

    hacer animacion

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,13 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+// import { trigger, state, style, animate, transition } from '@angular/animations'
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
+  // animations: [
+  //   state('open', style({
+  //     height: '200px',
+  //     opacity: 1,
+  //     backgroundColor: 'yellow'
+  //   })),
+  //   state('closed', style({
+  //     height: '100px',
+  //     opacity: 0.8,
+  //     backgroundColor: 'blue'
+  //   })),
+  // ]
 })
+
+
 export class NavComponent implements OnInit {
 
-  constructor() { }
+
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
   }
