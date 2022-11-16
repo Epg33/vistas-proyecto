31e770531435a35a5b58ba23e153645891df5f69
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-11-16 14:10:17 -0500

    mini animaciones

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,11 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations'
+
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  styleUrls: ['./landing.component.css'],
+  animations:[
+    trigger('view', [
+      state('in', style({
+        opacity: '1',
+        transform: 'translateY(-3vh)'
+
+      })),
+      state('out', style({
+        opacity: '0.2',
+        transform: 'translateY(30vh)'
+      })),
+      transition('in => out',[
+        animate('.5s')
+      ]),
+      transition('out => in',[
+        animate('.5s')
+      ])
+    ])
+  ]
 })
 export class LandingComponent implements OnInit {
+
+  move:boolean = false;
+
+  sight(){
+    if(window.scrollY > window.innerHeight*0.85){
+      this.move = true;
+    }
+    else {
+      this.move = false
+    }
+  }
 
   constructor() { }
 
